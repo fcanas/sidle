9287e13e7fed25e05ee6fc7190202579ca79917f
Fabián Cañas <fcanas@gmail.com>
2023-12-22 14:26:43 -0500

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/jdhealy/PrettyColors", from: "5.0.2"),
-		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.2"),
+		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
 	],
 	targets: [
 		.target(
